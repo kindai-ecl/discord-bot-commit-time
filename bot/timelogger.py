dacd9941eacb6684ff55465790491802432c1c01
K.Yamakawa <87867174+lCyou@users.noreply.github.com>
2024-05-12 03:50:48 +0900

    add: コミット時間の計算

--- bot/timelogger.py
+++ bot/timelogger.py
@@ -3,21 +3,20 @@ from db.settings import session
 from db.schema import User, TimeLog
 
 
+# ユーザーの合計時間を取得する
 def total_time(user_id: int):
-    # ユーザーの合計時間を取得する
     user = session.query(User).filter(User.user_id == user_id).first()
     return user.total_time
 
+# ユーザーの時間ログを追加する
 def stamp_time_log(user_id: int, timestamp: datetime, status: str):
-    # ユーザーの時間ログを追加する
     time_log = TimeLog(user_id=user_id, timestamp=timestamp, status=status)
     session.add(time_log)
     session.commit()
 
-def calc_total_time(user_id: int, start_time: datetime, end_time: datetime):
-    # ユーザーの合計時間を計算する
-    time_logs = session.query(TimeLog).filter(TimeLog.user_id == user_id).all()
-    total_time = timedelta()
-    for time_log in time_logs:
-        total_time += time_log.end_time - time_log.start_time
+# ユーザーの合計時間を計算する
+def update_total_time(user_id: int):
+    end = session.query(TimeLog).filter(TimeLog.user_id == user_id, TimeLog.status == "end").order_by(TimeLog.timestamp).first()
+    start = session.query(TimeLog).filter(TimeLog.user_id == user_id, TimeLog.status == "start").order_by(TimeLog.timestamp).first()
+    total_time = end.timestamp - start.timestamp
     return total_time
