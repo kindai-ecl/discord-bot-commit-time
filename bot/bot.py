707514559a5e40d18991e9da7731e73f3355c879
K.Yamakawa <87867174+lCyou@users.noreply.github.com>
2024-05-12 02:19:55 +0900

    add: タイムスタンプを押す

--- bot/bot.py
+++ bot/bot.py
@@ -6,7 +6,7 @@ from datetime import datetime, timedelta, timezone
 
 import sys
 sys.path.append("../")
-from timelogger import total_time
+import timelogger 
 
 ## botの使い方
 lines = []
@@ -27,27 +27,23 @@ tree = app_commands.CommandTree(client)
 @client.event
 async def on_ready():
     await tree.sync()
-    # await client.change_presence(activity=discord.Game(new_activity)) 
     print('ログインしました')
 
 @client.event
 async def on_voice_state_update(member, before, after): 
     now = datetime.now(JST)
-    if after.channel != None and '室' in after.channel.name:
-        print("%s : %sに参加しました",now, after.channel.name)
-        channel = client.get_channel(874633285047840768)
-        await channel.send(f"{member.name}が{after.channel.name}に参加しました")
-    elif before.channel != None and '室' in before.channel.name:
-        print("%s : %sを退出しました",now, before.channel.name)
-        channel = client.get_channel(874633285047840768)
-        await channel.send(f"{member.name}が{before.channel.name}を退出しました")
-    
 
+    if after.channel != None and '室' in after.channel.name:
+        timelogger.stamp_time_log(member.id, now, 'start')
+
+    elif before.channel != None and '室' in before.channel.name:
+        timelogger.stamp_time_log(member.id, now, 'end')
+
+### コマンド実装部分 ###
 
 @tree.command(name="total_time",description="合計時間を表示します")
 async def total_time(interaction: discord.Interaction, user:discord.Member):
     await interaction.response.send_message(user.name + "の合計時間はです",ephemeral=True)
-
 
 @tree.command(name="help",description="ヘルプを表示します")
 async def help(interaction: discord.Interaction):
