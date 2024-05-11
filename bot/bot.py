cbc618d44f2981c2b034facbf4e570e91a6d8ed6
K.Yamakawa <87867174+lCyou@users.noreply.github.com>
2024-05-12 03:51:19 +0900

    chore: 新しいコマンド増設

--- bot/bot.py
+++ bot/bot.py
@@ -30,6 +30,9 @@ async def on_ready():
     print('ログインしました')
 
 @client.event
+
+
+@client.event
 async def on_voice_state_update(member, before, after): 
     now = datetime.now(JST)
 
@@ -38,12 +41,21 @@ async def on_voice_state_update(member, before, after):
 
     elif before.channel != None and '室' in before.channel.name:
         timelogger.stamp_time_log(member.id, now, 'end')
+        timelogger.update_total_time(member.id)
 
 ### コマンド実装部分 ###
+
+@tree.command(name="weekly_commit",description="直近1週間のログを表示します")
+async def weekly_commit(interaction: discord.Interaction, user:discord.Member):
+    await interaction.response.send_message(user.name + "の直近1週間のログはです",ephemeral=True)
 
 @tree.command(name="total_time",description="合計時間を表示します")
 async def total_time(interaction: discord.Interaction, user:discord.Member):
     await interaction.response.send_message(user.name + "の合計時間はです",ephemeral=True)
+
+@tree.command(name="auto_role",description="自動ロールを設定します")
+async def auto_role(interaction: discord.Interaction):
+    await interaction.response.send_message("自動ロールを設定しました",ephemeral=True)
 
 @tree.command(name="help",description="ヘルプを表示します")
 async def help(interaction: discord.Interaction):
