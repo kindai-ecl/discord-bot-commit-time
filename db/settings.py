9c11cc4b052e972f6ade980a4edc30737326bca5
K.Yamakawa <87867174+lCyou@users.noreply.github.com>
2024-06-15 14:59:58 +0900

    refactor: lintの適応

--- db/settings.py
+++ db/settings.py
@@ -1,28 +1,20 @@
-import os 
+
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import scoped_session, sessionmaker
 from sqlalchemy import event
 
 # DBの接続
-DATABASE = 'sqlite:///../db/timelog.db'
+DATABASE = "sqlite:///../db/timelog.db"
 
-Engine = create_engine(
-    DATABASE,
-    echo=True
-)
+Engine = create_engine(DATABASE, echo=True)
 Base = declarative_base()
 
-session = scoped_session(
-    sessionmaker(
-        autocommit = False,
-	    autoflush = True,
-	    bind = Engine
-    )
-)
+session = scoped_session(sessionmaker(autocommit=False, autoflush=True, bind=Engine))
 
 Base = declarative_base()
 Base.query = session.query_property()
+
 
 @event.listens_for(Engine, "connect")
 def set_sqlite_pragma(dbapi_connection, connection_record):
