364a296f03e5cea29ac81c9f807ce9b320056da6
K.Yamakawa <87867174+lCyou@users.noreply.github.com>
2024-06-15 22:00:54 +0900

    refactor: ディレクトリ構造を推奨される階層に置き換え

--- bot/db/timelogger.py
+++ bot/db/timelogger.py
@@ -4,6 +4,7 @@ from sqlalchemy import desc, text
 from db.settings import session
 from db.schema import User, TimeLog
 
+
 # ユーザーを追加する
 def register_user(user_id: int, name: str):
     if authorized(user_id):
@@ -13,6 +14,7 @@ def register_user(user_id: int, name: str):
     session.commit()
     return "登録しました"
 
+
 # ユーザーが存在するか確認する
 def authorized(user_id: int):
     user = session.query(User).filter(User.user_id == user_id).first()
@@ -20,17 +22,21 @@ def authorized(user_id: int):
         return True
     return False
 
+
 # ユーザーの直近1週間の時間ログを取得する
 def weekly_commit(user_id: int, start: datetime):
     user = session.query(User).filter(User.user_id == user_id).first()
     if user is None:
         return "ありません。登録が必要です。\n/register コマンドで登録してください"
-    query = text("SELECT timestamp, status FROM time_logs WHERE user_id = :user_id and :start < timestamp;")
+    query = text(
+        "SELECT timestamp, status FROM time_logs WHERE user_id = :user_id and :start < timestamp;"
+    )
     employee_list = session.execute(query, {"user_id": user_id, "start": start})
     array = []
     for row in employee_list:
         array.append(f"{row[0]}: {row[1]}")
     return array
+
 
 # ユーザーの合計時間を取得する
 def total_time(user_id: int):
@@ -41,20 +47,32 @@ def total_time(user_id: int):
     hour = user.total_min // 60
     return f"{hour}時間{min}分です"
 
+
 # ユーザーの時間ログを追加する
 def stamp_time_log(user_id: int, timestamp: datetime, status: str):
     time_log = TimeLog(user_id=user_id, timestamp=timestamp, status=status)
     session.add(time_log)
     session.commit()
 
+
 # ユーザーの合計時間を計算する
 def update_total_time(user_id: int):
-    end = session.query(TimeLog).filter(TimeLog.user_id == user_id, TimeLog.status == "end").order_by(desc(TimeLog.timestamp)).first()
-    start = session.query(TimeLog).filter(TimeLog.user_id == user_id, TimeLog.status == "start").order_by(desc(TimeLog.timestamp)).first()
+    end = (
+        session.query(TimeLog)
+        .filter(TimeLog.user_id == user_id, TimeLog.status == "end")
+        .order_by(desc(TimeLog.timestamp))
+        .first()
+    )
+    start = (
+        session.query(TimeLog)
+        .filter(TimeLog.user_id == user_id, TimeLog.status == "start")
+        .order_by(desc(TimeLog.timestamp))
+        .first()
+    )
     total_time = end.timestamp - start.timestamp
     commit_time = total_time // timedelta(minutes=1)
 
     user = session.query(User).filter(User.user_id == user_id).first()
     user.total_min += commit_time
     session.commit()
-    return 
+    return
